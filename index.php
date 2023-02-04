efcb36a83f34daa6274cf2aefea280b97c9526c8
4ks-devops <NO-REPLY@4KS.MX>
2023-02-04 16:05:14 -0600

    demo1

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="shortcut icon" href="#" />
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>PORTAL PRIVADO</title>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="estilos.css">
+        <link rel="stylesheet" href="plugins/sweetalert2/sweetalert2.min.css">        
+        
+        <link rel="stylesheet" type="text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">
+        
+    </head>
+    
+    <body>
+     
+      <div class="container-login">
+        <div class="wrap-login">
+            <form class="login-form validate-form" id="formLogin" action="" method="post">
+                <span class="login-form-title">ANTONIO SOLIS ALEMAN</span>
+                <div class="pt-3 mt-3">
+                    <img src="img/user.png" alt="" class="img-responsive imgUsuario">
+                    
+                </div>
+                <div class="wrap-input100" data-validate = "Usuario incorrecto">
+                    <input class="input100" type="text" id="usuario" name="usuario" placeholder="Usuario">
+                    <span class="focus-efecto"></span>
+                </div>
+                
+                <div class="wrap-input100" data-validate="Password incorrecto">
+                    <input class="input100" type="password" id="password" name="password" placeholder="Password">
+                    <span class="focus-efecto"></span>
+                </div>
+                
+                <div class="container-login-form-btn">
+                    <div class="wrap-login-form-btn">
+                        <div class="login-form-bgbtn"></div>
+                        <button type="submit" name="submit" class="login-form-btn">CONECTAR</button>
+                    </div>
+                </div>
+                    <div class="form-field align-items-center pb-3 pt-3 mb-4">
+                        <select name="opcionesLogin" id="opcionesLogin" class="form-select">
+                            <option value="">Selecciona</option>
+                            <option value="educacion0" class="primerNivel" href="https://pdv-csn.apps-crc.testing" >140-Csn finanzas</option>
+                            <option value="141" class="segundoNivel" href="app1.4ks.online">141-Csn Mail</option>
+                            <option value="143" class="segundoNivel" href="app2.4ks.online">143-App Arco</option>
+                            <option value="145" class="segundoNivel" href="app3.4ks.online">145-App_Sgi</option>
+                            <option value="159" class="segundoNivel" href="app4.4ks.online">159-Políticas Csn</option>
+                        </select>
+                    </div>
+            </form>
+        </div>
+    </div>     
+        
+        
+     <script src="jquery/jquery-3.3.1.min.js"></script>    
+     <script src="bootstrap/js/bootstrap.min.js"></script>    
+     <script src="popper/popper.min.js"></script>    
+        
+     <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>    
+     <script src="codigo.js"></script>    
+    </body>
+</html>
